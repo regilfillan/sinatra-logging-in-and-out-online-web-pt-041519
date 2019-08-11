de264cfbb647bd4ea03c83ac9bb597d249c36e27
Ryan Gilfillan <regilfillan@gmail.com>
2019-08-11 18:43:52 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'rack/test'
 require 'capybara/rspec'
 require 'capybara/dsl'
 
-if ActiveRecord::Base.connection.migration_key.needs_migration?
+if ActiveRecord::Base.connection.migration_keys.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
 end
 
